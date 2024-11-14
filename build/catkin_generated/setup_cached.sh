181d7f29c4774a3a6ca503f597a0f9a5dc15ed34
Eugene Ng <eugene1862@gmail.com>
2024-11-14 11:24:12 -0800

    catkin_make

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa24/class/ee106a-abn/Poolbot/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/cc/ee106a/fa24/class/ee106a-abn/Poolbot/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cc/ee106a/fa24/class/ee106a-abn/Poolbot/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa24/class/ee106a-abn/Poolbot/src:$ROS_PACKAGE_PATH"
