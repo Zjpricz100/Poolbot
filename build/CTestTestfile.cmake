953f1480ba4ba1b6760c42d076bed757010ad715
zach pricz <zpricz@berkeley.edu>
2024-11-15 17:37:34 -0800

    Reorganized

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106a/fa24/class/ee106a-add/ros_workspaces/Poolbot/src
-# Build directory: /home/cc/ee106a/fa24/class/ee106a-add/ros_workspaces/Poolbot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
