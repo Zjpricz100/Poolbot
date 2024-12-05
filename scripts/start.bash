eced7f204ac8fee617a511ea83a21f56ed8923c2
Eugene Ng <eugene1862@gmail.com>
2024-12-04 23:12:28 -0800

    updated scripts

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,2 +1,4 @@
 xterm -hold -e 'rosrun intera_interface joint_trajectory_action_server.py' &
 xterm -hold -e 'roslaunch sawyer_moveit_config sawyer_moveit.launch electric_gripper:=true' &
+xterm -hold -e 'cd ..; source devel/setup.bash; roslaunch lab4_cam ar_track.launch' &
+xterm -hold -e 'cd ..; source devel/setup.bash; roslaunch lab4_cam run_cam.launch' &
