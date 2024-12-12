9019119e85564912cbb348e234dac55ff0c22fad
Eugene Ng <eugene1862@gmail.com>
2024-12-11 18:27:34 -0800

    packages.txt

--- src/vision/object_detector.py
+++ src/vision/object_detector.py
@@ -14,8 +14,6 @@ import tf2_geometry_msgs
 from geometry_msgs.msg import Point, PoseStamped, TransformStamped
 from std_msgs.msg import Header
 from table_log import TableLog
-from tf_transformations import quaternion_matrix, quaternion_from_matrix
-
 
 PLOTS_DIR = os.path.join(os.getcwd(), 'plots')
 
@@ -32,8 +30,8 @@ class ObjectDetector:
         self.cx = None
         self.cy = None
 
-        self.usb_camera_info_sub = rospy.Subscriber("/usb_cam/camera_info", CameraInfo, self.usb_camera_info_callback)
-        self.head_camera_info_sub = rospy.Subscriber("/io/internal_camera/head_camera/camera_info", CameraInfo, self.head_camera_info_callback)
+        self.wrist_camera_info_sub = rospy.Subscriber("/io/internal_camera/right_hand_camera/camera_info", CameraInfo, self.usb_camera_info_callback)
+        #self.head_camera_info_sub = rospy.Subscriber("/io/internal_camera/head_camera/camera_info", CameraInfo, self.head_camera_info_callback)
         
         self.tfBuffer = tf2_ros.Buffer()
         self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
@@ -54,7 +52,8 @@ class ObjectDetector:
             "black" : rospy.Publisher("ball/black", self.message_type, queue_size=10),
             "green" : rospy.Publisher("ball/green", self.message_type, queue_size=10)
         }
-        self.color_image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.color_image_callback)
+        #self.color_image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.color_image_callback)
+        self.color_image_sub = rospy.Subscriber("/io/internal_camera/right_hand_camera/image_rect", Image, self.color_image_callback)
 
         rospy.spin()
 
@@ -125,17 +124,19 @@ class ObjectDetector:
             
             # Convert the ROS Image message to an OpenCV image (BGR8 format)
             frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            # cv2.imshow("iamg", frame)
+            # cv2.waitKey(1)
             ball_dict = self.detect_balls(frame)
-            for (key, value) in ball_dict.items():
-                pub = self.pubs[key]
-                # ball = PoseStamped()
-                # ball.pose.position.x = value[0]
-                # ball.pose.position.y = value[1]
-                # pub.publish(ball)
-                Z = self.get_table_height()
-                x, y = value
-                ball = self.pixel_to_world(x, y, Z)
-                pub.publish(ball)
+            # for (key, value) in ball_dict.items():
+            #     pub = self.pubs[key]
+            #     # ball = PoseStamped()
+            #     # ball.pose.position.x = value[0]
+            #     # ball.pose.position.y = value[1]
+            #     # pub.publish(ball)
+            #     Z = self.get_table_height()
+            #     x, y = value
+            #     ball = self.pixel_to_world(x, y, Z)
+            #     pub.publish(ball)
 
         except Exception as e:
             print("Error:", e)
@@ -185,86 +186,42 @@ class ObjectDetector:
         return ball_in_base_frame
 
     def detect_balls(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Apply GaussianBlur to reduce noise and improve circle detection
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        blurred = cv2.GaussianBlur(frame, (5, 5), 0)
 
         # Detect circles using HoughCircles
         circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.3, minDist=30,
-                                    param1=100, param2=30, minRadius=15, maxRadius=25)
+                                    param1=100, param2=30, minRadius=0, maxRadius=35)
 
         # If some circles are detected
         if circles is not None:
             # Round the circle values and convert to integer
             circles = np.round(circles[0, :]).astype("int")
-
-            # Define HSV ranges for target colors
-            color_ranges = {
-                "white": ([0, 0, 200], [180, 30, 255]),
-                "yellow": ([20, 100, 100], [30, 255, 255]),
-                "blue": ([70, 0, 50], [120, 255, 255]),
-                "red": ([0, 0, 0], [5, 255, 255]),
-                "purple": ([110, 0, 0], [179, 255, 255]),
-                "orange": ([10, 100, 0], [20, 255, 255]),
-                "maroon": ([0, 100, 0], [18, 240, 255]),
-                "black": ([0, 0, 0], [180, 255, 50]),
-                "green": ([40, 100, 100], [80, 255, 255]),
-            }
             ball_dict = {}
 
             # Loop through the circles and process each one
-            for (x, y, r) in circles:
+            for i, (x, y, r) in enumerate(circles):
                 # Draw the circle in green
                 cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
                 # Draw the center of the circle in red
                 cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)
 
-                # Define bounding box for ROI
-                top_left_x = max(x - r, 0)
-                top_left_y = max(y - r, 0)
-                bottom_right_x = min(x + r, frame.shape[1])
-                bottom_right_y = min(y + r, frame.shape[0])
+                cv2.putText(
+                    frame,
+                    f"ball_{i}",
+                    (x, y - r - 10),  # Position above the circle
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    2
+                )
+                ball_dict[i] = (x, y) # populate ball dict with (x, y) values
 
-                # Extract the region of interest (ROI)
-                roi = frame[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
-
-                if roi.size > 0:
-                    # Convert ROI to HSV
-                    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-                    # Find the closest matching color
-                    max_percentage = 0
-                    detected_color = "unknown"
-
-                    for color, (lower_hsv, upper_hsv) in color_ranges.items():
-                        lower_hsv = np.array(lower_hsv, dtype="uint8")
-                        upper_hsv = np.array(upper_hsv, dtype="uint8")
-                        mask = cv2.inRange(hsv_roi, lower_hsv, upper_hsv)
-
-                        # Calculate the percentage of the ROI matching this color
-                        color_percentage = np.sum(mask) / mask.size
-
-                        if color_percentage > max_percentage:
-                            max_percentage = color_percentage
-                            detected_color = color
-
-                    # If a color is detected, display it on the frame
-                    if max_percentage > 0.1:  # Threshold to avoid noise
-                        #print(f"{detected_color.capitalize()} ball detected at ({x}, {y}) with radius {r}")
-                        cv2.putText(
-                            frame,
-                            detected_color,
-                            (x, y - r - 10),  # Position above the circle
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5,
-                            (255, 0, 0),
-                            2
-                        )
-                        ball_dict[detected_color] = (x, y) # populate ball dict with (x, y) values
             cv2.imshow("Circle Detection", frame)
             cv2.waitKey(1)
-            return ball_dict
+        #return ball_dict
 
 
     def get_rot_and_translation(self, ar_tag):
